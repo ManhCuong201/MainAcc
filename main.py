d03450c224bc5060c093bd69d545d131d12a9d3d
noname201012345 <58730410+noname201012345@users.noreply.github.com>
2022-12-09 14:07:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -36,14 +36,11 @@ async def on_ready():
 
 @client.event
 async def on_call():
-    try:
-        while True:
-            await asyncio.sleep(1)
-            if not client.get_guild(GUILD_ID).get_member(client.user.id).voice:
-                vc = client.get_guild(GUILD_ID).get_channel(CHANNEL_ID)
-                await vc.connect()
-    except:
-        pass
+    while True:
+        await asyncio.sleep(1)
+        if not client.get_guild(GUILD_ID).get_member(client.user.id).voice:
+            vc = client.get_guild(GUILD_ID).get_channel(CHANNEL_ID)
+            await vc.connect()
     client.dispatch("call")
 
 client.run(os.getenv("TOKEN"))
